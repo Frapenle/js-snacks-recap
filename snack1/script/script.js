3e205d408e6f9cf0fa004ff59f04060a563beac3
francescotrudu <francescotrudu84@gmail.com>
2022-12-22 18:44:10 +0100

    Snack1

---
+++ snack1/script/script.js
@@ -0,0 +1,37 @@
+/**
+Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
+ Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
+*/
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            randomNumber: null,
+            oddNumbers: [],
+            evenNumbers: [],
+        }
+    },
+
+    // methods
+    methods: {
+        getANumber() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+                // arrow function reference 'this' parent
+                .then((response) => {
+                    const result = response.data.response;
+                    this.randomNumber = result
+                        if (this.randomNumber % 2 === 0) {
+                            this.oddNumbers.push(result)
+                        } else {
+                            this.evenNumbers.push(result)
+                        }
+                });
+        },
+    },
+    //call function at every reload page
+    created() {
+    }
+
+}).mount('#app')
+
